d77423e61bbf6c46b8de953a322d62eb1b6b956b
Joseph Madich <joseph.madich@LOH070W374ZHTD6.cardinalhealth.net>
2018-09-12 10:14:48 -0400

    adding react-bootstrap components, css reset, and updating to bootstrap3 layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 import logo from '../logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="App">
+        <Navbar className="App-nav" inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a className="App-nav-title" href="/"><img src={logo} className="App-logo" alt="logo" /> React App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+        <Jumbotron>
+          <Grid>
+            <h1>Welcome to React</h1>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank">
+                View React Bootstrap Docs
+              </Button>
+            </p>
+          </Grid>
+        </Jumbotron>
+      </section>
     );
   }
 }
